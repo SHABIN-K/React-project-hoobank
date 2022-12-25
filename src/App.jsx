cd90619dfbdd93ef928c6f1cb5d1b97618db4a9b
Shabin-k <helloshabink@gmail.com>
2022-12-25 19:17:58 +0530

    complete stats section

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
+
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
         <Navbar/>
@@ -25,12 +26,13 @@ const App = () => (
           <Hero/>
         </div>
       </div>
-      
+
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-         
+         <Stats/>
         </div>
       </div>
+      
     </div>
   );
 
