f165edd89289640a4d218365f51963c9d081660e
Shabin-k <helloshabink@gmail.com>
2022-12-25 14:26:25 +0530

    complete navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import styles from './style';
-
+import {
+  Navbar,
+  Billing,
+  CardDeals,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero} from './Components'
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-        navbar
+        <Navbar/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          bussiness
-          billing 
-          carddeal
-          testimonials
-          clients
-          Footer
+         
         </div>
       </div>
     </div>
